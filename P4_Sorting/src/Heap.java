4c11d733cb1cf568a0c91d3b4429cc7a12c20e3d
Joseph Volpe <jlv38@case.edu>
2015-04-17 01:05:53 -0400

    Fourth project

--- P4_Sorting/src/Heap.java
+++ P4_Sorting/src/Heap.java
@@ -1,27 +1,66 @@
 /**
- * Heap to be used for HeapSort
+ * Heap to be used with HeapSort
  * @author josephvolpe
  *
  */
 public class Heap {
-
+	
 	/** Array to represent the heap */
-	int[] heap;
+	private int[] heap;
 	
-	public void buildHeap() {
-		
+	/** Current number of items in the heap */
+	private int numItems;
+	
+	/**
+	 * Create heap with input array
+	 * @param arr array to create heap with
+	 */
+	public Heap(int[] arr) {
+		heap = arr;
+		numItems = arr.length;
 	}
 	
-	public void heapify() {
-		
+	/**
+	 * Build a max on top heap
+	 */
+	public void buildMaxOnTopHeap() {
+		for (int i = numItems / 2; i >= 0; i--) //for all leaf nodes to the root
+			maxHeapify(i);					 
 	}
 	
-	public void siftDown() {
-		
+	/**
+	 * Sifts down any node where the children are greater to be able to 
+	 * keep the principle of a max on top heap
+	 * @param i
+	 */
+	public void maxHeapify(int i) {
+		int toSift = heap[i];                 //get value at root
+		int parent = i;						  //index of parent
+		int child = (2 * parent) + 1;	      //index of left child
+		while (child < numItems) {
+			if ((child + 1 < numItems) && (heap[child] < heap[child + 1]))
+				child = child + 1;            //makes sure to use greater child
+			if (heap[parent] < heap[child]) { //swap child with parent
+				heap[parent] = heap[child];
+				heap[child] = toSift;
+				parent = child;
+				child = parent * 2 + 1;
+			}
+			else  							  //break if the parent is the greatest
+				break;
+		}
+		heap[parent] = toSift;
 	}
 	
+	/**
+	 * Return max and remove it from the heap
+	 * @return max
+	 */
 	public int removeMax() {
-		
+		int max = heap[0];              
+		heap[0] = heap[numItems - 1];        //switch last node to first
+		numItems--; 							
+		maxHeapify(0);					     //heapify now that its out of order
+		return max;
 	}
-	
 }
